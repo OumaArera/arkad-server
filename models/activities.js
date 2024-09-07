f422e7427744717e08509e30e4fcea7340a4a116
OumaArera <johnouma999@gmail.com>
2024-09-07 21:08:35 +0300

    feat: create volunteers

--- models/activities.js
+++ models/activities.js
@@ -1,46 +1,52 @@
 module.exports = (sequelize, DataTypes) => {
-    const Activity = sequelize.define('Activity', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+  const Activity = sequelize.define('Activity', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', 
+        key: 'id',
       },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users', 
-          key: 'id',
-        },
-        onDelete: 'CASCADE',
-      },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      venue: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-    }, {
-      timestamps: true,
+      onDelete: 'CASCADE',
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    venue: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    timestamps: true,
+  });
+
+  // Add this part
+  Activity.associate = (models) => {
+    Activity.belongsTo(models.User, { 
+      foreignKey: 'userId', 
+      as: 'user' 
     });
-  
-    Activity.associate = (models) => {
-        Activity.belongsTo(models.User, { 
-        foreignKey: 'userId', 
-        as: 'user' 
-      });
-    };
-  
-    return Activity;
+
+    // The missing association: Activity has many Volunteers
+    Activity.hasMany(models.Volunteer, {
+      foreignKey: 'activityId',
+      as: 'volunteers',  // Alias for eager loading
+    });
   };
-  
+
+  return Activity;
+};
