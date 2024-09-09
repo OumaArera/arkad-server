6c499cf90ca287b8c4887f3f5aea37f2e346516a
OumaArera <johnouma999@gmail.com>
2024-09-09 21:55:52 +0300

    feat: payment

--- payment/donate.js
+++ payment/donate.js
@@ -54,34 +54,34 @@ const generateAccessToken = async () => {
 };
 
 router.post('/', async (req, res) => {
-  // const { iv, ciphertext } = req.body;
-  const { phone, fullName, email, amount } = req.body;
+  const { iv, ciphertext } = req.body;
+  // const { phone, fullName, email, amount } = req.body;
 
-  // if (!iv || !ciphertext) {
-  //   return res.status(400).json({
-  //     success: false,
-  //     message: 'Invalid data. Missing required fields',
-  //     statusCode: 400,
-  //   });
-  // }
+  if (!iv || !ciphertext) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid data. Missing required fields',
+      statusCode: 400,
+    });
+  }
 
   try {
-    // const key = process.env.ENCRYPTION_KEY;
-    // if (!key) {
-    //   throw new Error('Missing required keys');
-    // }
+    const key = process.env.ENCRYPTION_KEY;
+    if (!key) {
+      throw new Error('Missing required keys');
+    }
 
-    // const decryptedBytes = CryptoJS.AES.decrypt(ciphertext, CryptoJS.enc.Utf8.parse(key), {
-    //   iv: CryptoJS.enc.Hex.parse(iv),
-    //   padding: CryptoJS.pad.Pkcs7,
-    //   mode: CryptoJS.mode.CBC,
-    // });
-    // let decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
-    // decryptedData = decryptedData.replace(/\0+$/, '');
+    const decryptedBytes = CryptoJS.AES.decrypt(ciphertext, CryptoJS.enc.Utf8.parse(key), {
+      iv: CryptoJS.enc.Hex.parse(iv),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+    let decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
+    decryptedData = decryptedData.replace(/\0+$/, '');
 
-    // const userData = JSON.parse(decryptedData);
-    // Object.entries(userData).forEach(([key, value]) => console.log(`${key} : ${value}`));
-    // const { phone, fullName, email, amount } = userData;
+    const userData = JSON.parse(decryptedData);
+    Object.entries(userData).forEach(([key, value]) => console.log(`${key} : ${value}`));
+    const { phone, fullName, email, amount } = userData;
 
     if(!phone || !fullName || !email || !amount){
         return res.status(400).json({
