a227f95abf969c8e33d0e9cffc40092f86d861d3
OumaArera <johnouma999@gmail.com>
2024-11-02 14:42:34 +0300

    feat: complete

--- authentication/changePassword.js
+++ authentication/changePassword.js
@@ -1,49 +1,27 @@
 const express = require('express');
 const db = require('../models');
 const bcrypt = require('bcrypt');
-const CryptoJS = require('crypto-js');
 const authenticateToken = require("./authenticateToken");
 require('dotenv').config();
 
 const router = express.Router();
 
 router.put('/', authenticateToken, async (req, res) => {
-  const { iv, ciphertext } = req.body;
+  const { username, oldPassword, newPassword } = req.body;
 
-  if (!iv || !ciphertext) {
+  // Validate required fields
+  if (!username || !oldPassword || !newPassword) {
     return res.status(400).json({
       success: false,
-      message: 'Invalid data. Missing required fields',
+      message: 'Username, old password, and new password are required',
       statusCode: 400,
     });
   }
 
   try {
-    const key = process.env.ENCRYPTION_KEY;
     const saltKey = process.env.SALTING_KEY;
-
-    if (!key || !saltKey) {
-      throw new Error('Missing encryption or salting keys');
-    }
-
-    // Decrypt the incoming data
-    const decryptedBytes = CryptoJS.AES.decrypt(ciphertext, CryptoJS.enc.Utf8.parse(key), {
-      iv: CryptoJS.enc.Hex.parse(iv),
-      padding: CryptoJS.pad.Pkcs7,
-      mode: CryptoJS.mode.CBC,
-    });
-    let decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
-    decryptedData = decryptedData.replace(/\0+$/, '');  // Remove trailing null bytes
-
-    const userData = JSON.parse(decryptedData);
-    const { username, oldPassword, newPassword } = userData;
-
-    if (!username || !oldPassword || !newPassword) {
-      return res.status(400).json({
-        success: false,
-        message: 'Username, old password, and new password are required',
-        statusCode: 400,
-      });
+    if (!saltKey) {
+      throw new Error('Missing salting key');
     }
 
     // Check if the user exists
@@ -59,7 +37,7 @@ router.put('/', authenticateToken, async (req, res) => {
     // Verify the old password
     const saltedOldPassword = oldPassword + saltKey;
     const isOldPasswordValid = await bcrypt.compare(saltedOldPassword, user.password);
-    
+
     if (!isOldPasswordValid) {
       return res.status(401).json({
         success: false,
